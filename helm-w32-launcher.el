b77b6e10dc1057f13d2f159bd7d2765c7e67281a
Fanael Linithien <fanael4@gmail.com>
2014-12-16 22:40:16 +0100

    Split the C# code: one file per class.

--- helm-w32-launcher.el
+++ helm-w32-launcher.el
@@ -8,29 +8,7 @@
 ;; This file is NOT part of GNU Emacs.
 
 ;; Copyright (c) 2014, Fanael Linithien
-;; All rights reserved.
-;;
-;; Redistribution and use in source and binary forms, with or without
-;; modification, are permitted provided that the following conditions are
-;; met:
-;;
-;;   * Redistributions of source code must retain the above copyright
-;;     notice, this list of conditions and the following disclaimer.
-;;   * Redistributions in binary form must reproduce the above copyright
-;;     notice, this list of conditions and the following disclaimer in the
-;;     documentation and/or other materials provided with the distribution.
-;;
-;; THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
-;; IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
-;; TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
-;; PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER
-;; OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
-;; EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
-;; PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
-;; PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
-;; LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
-;; NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
-;; SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+;; See license.txt for licensing information.
 
 ;;; Commentary:
 
@@ -182,9 +160,9 @@ ACTION is the function to call upon selecting a candidate."
 (defconst helm-w32-launcher--package-directory
   (file-name-directory (or load-file-name default-directory)))
 (defconst helm-w32-launcher--helper-source
-  (expand-file-name "StartMenuItems.cs" helm-w32-launcher--package-directory))
+  (expand-file-name "helper-src/*" helm-w32-launcher--package-directory))
 (defconst helm-w32-launcher--helper-name
-  (expand-file-name "StartMenuItems.exe" helm-w32-launcher--package-directory))
+  (expand-file-name "helper.exe" helm-w32-launcher--package-directory))
 
 (defun helm-w32-launcher--call-helper-list-items ()
   "Call the helper program to get the list of Start Menu items."
